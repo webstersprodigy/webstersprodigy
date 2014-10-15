8bcfbed9f1fa6b1a8dd398a728dcb2daaa376b8b
webstersprodigy <richard.lundeen@gmail.com>
2014-10-15 14:19:38 -0700

    Actual copy paste license checkin

--- burp/cryptoAttack.py
+++ burp/cryptoAttack.py
@@ -1,6 +1,5 @@
 
-# Author: James Kettle <james.kettle@contextis.co.uk>
-# Copyright 2014 Context Information Security
+# Author: Rich LUndeen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
